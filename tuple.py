c2f3e5ac77d767200ca58c7c76ad88c22ded7c9b
Himanshu-Tiwari2001 <tiwarihimanshu.ht2001@gmail.com>
2025-01-02 12:20:02 +0530

    adding new Concept about tuple

--- tuple.py
+++ tuple.py
@@ -1,6 +1,6 @@
-tuple=(1,2,8,7,41,65,32,23,"a","b","c","d","e","f")
-print (tuple)
-print(type(tuple))
+tuple1=(1,2,8,7,41,65,32,23,"a","b","c","d","e","f")
+print (tuple1)
+print(type(tuple1))
 
 #Sorting the tuple elements using the sorted() function in acending order
 number=(1,2,8,7,41,65,32,23)
@@ -58,15 +58,25 @@ print("Max value from tuple is ",max(order_online)) # Pizza
 
 # note since the tuple is immutable ,a new element cnnot be modified
 price=(100,200,250,350,450)
-del price[0]
+# del price[0]
 
  
 # we can convert the tuple to a list and then modify it and then convert it back to a tuple
-
-price_list=list(price)
+price1=(100,200,250,350,450)
+price_list=list(price1)
 price_list[0]=150
-price=tuple(price_list)
-print("Modified price tuple is ",price)
+price1=tuple(price_list)
+
+print("Modified price tuple is ",price1)
+
+# converting tuple to dictionary
+purchase_item=("Sanatizer","Chocolate","Biscuit","Brush")
+
+purchase_price=("20","30","15","10")
+
+# to do that we have to use zip() function in python it combine two tuple it takes the two tuple as  parameter/Argumnent and return dictionary having key and value
+purchase_dict=dict(zip(purchase_item,purchase_price))
+print("The converted tuple",purchase_dict)
 
 #Did You Know? Because tuple is immutable, a tuple needs lesser memory than lists.
 #Did You Know? Lesser memory fragmentation is better in terms of performance. Python can reuse a tuple which results in reduction of memory fragmentation and can speed up memory allocations. If a tuple is no longer required and has less than 20 elements, instead of deleting it permanently, Python reserves it temporarily.
