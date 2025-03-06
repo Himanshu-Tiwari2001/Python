862ddc3816581a933ccd6297cbdd620ac18c6a30
Himanshu-Tiwari2001 <tiwarihimanshu.ht2001@gmail.com>
2025-03-07 00:30:33 +0530

    .

---
+++ operators.py
@@ -0,0 +1,41 @@
+# identity operator 
+# it check whether the variable refers to same objects or not
+x="Vikash"
+y="Ramesh"
+
+print(x is y) # False
+
+# is not operator
+
+print(x is not y) # True
+
+# menbership operator
+
+list1=[1,2,3,4,5]
+
+print(5 in list1) # True
+
+print(6 not in list1) # True
+
+# bitwise operators
+print("The &/And operator")
+print("The binary of 15 is ",bin(15) )
+print("The binary of 10 is ",bin(10) )
+
+c=15 & 10
+print(f" The binary of {c} is ",bin(5) )
+
+print("The or / | operator")
+
+print("The binary of 15 is ",bin(15) )
+
+print("The binary of 10 is ",bin(10) )
+
+d=15 | 10
+
+print(f" The binary of {d} is ",bin(15) )
+
+# type casting 
+ram =2500 
+print(type(ram))
+incentive
